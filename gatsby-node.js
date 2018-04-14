333e5ffb7aa10c638be1cf435c50e24d0f0c715b
Unknown <ben.lonsdale@bluewren.co.uk>
2018-04-14 13:58:01 +0100

    fix build errors

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,10 @@
  */
 
  // You can delete this file if you're not using it
+
+exports.modifyBabelrc = ({ babelrc }) => ({ 
+    ...babelrc, 
+    plugins: babelrc.plugins.concat(
+        ['transform-regenerator']
+    ) 
+})
